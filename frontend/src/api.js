91f25c0ac3cb09d0fe9edaf594fe04ecd9d706e7
Shekar Ramaswamy <shekar@slide.so>
2023-04-26 15:16:42 -0700

    Transaction toasts

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,20 +1,43 @@
-import { transactionToast, errorToast } from "./components/alerts";
+import { errorToast } from "./components/alerts";
+import { toast } from "react-toastify";
 // Smart contract interactions
 
 export async function createGame(contract, connectedAddress, opponentAddress) {
   if (connectedAddress === "") {
     return;
   }
+
+  // TODO: better abstraction for transaction toasts
+  let id;
   try {
     const transaction = await contract.newGame(
       connectedAddress,
       opponentAddress
     );
-    console.log(transaction);
-    await transactionToast(transaction.hash, transaction.wait);
+    id = toast.loading(`Waiting for txn ${transaction.hash} to confirm`);
+
+    await transaction.wait();
+
+    toast.update(id, {
+      render: "Success!",
+      type: "success",
+      isLoading: false,
+      autoClose: 3000,
+    });
   } catch (error) {
-    errorToast("Error creating game");
     console.error("Error creating game: ", error);
+    if (!id) {
+      errorToast("Error creating game");
+      return;
+    }
+
+    // If the toast already exists, update it
+    toast.update(id, {
+      render: "Error creating game",
+      type: "error",
+      isLoading: false,
+      autoClose: 3000,
+    });
   }
 }
 
@@ -22,12 +45,35 @@ export async function markSpace(contract, connectedAddress, gameId, space) {
   if (connectedAddress === "") {
     return;
   }
+
+  let id;
   try {
     const transaction = await contract.markSpace(gameId, space);
-    await transactionToast(transaction.hash, transaction.wait);
+
+    id = toast.loading(`Waiting for txn ${transaction.hash} to confirm`);
+
+    await transaction.wait();
+
+    toast.update(id, {
+      render: "Success!",
+      type: "success",
+      isLoading: false,
+      autoClose: 3000,
+    });
   } catch (error) {
-    errorToast("Error marking space");
     console.error("Error marking space: ", error);
+    if (!id) {
+      errorToast("Error marking space");
+      return;
+    }
+
+    // If the toast already exists, update it
+    toast.update(id, {
+      render: "Error marking space",
+      type: "error",
+      isLoading: false,
+      autoClose: 3000,
+    });
   }
 }
 
--- frontend/src/components/alerts.js
+++ frontend/src/components/alerts.js
@@ -4,23 +4,3 @@ export const errorToast = (text) =>
   toast.error(text, {
     autoClose: 3000,
   });
-
-export const transactionToast = (txHash, fn) => {
-  toast.promise(fn, {
-    success: {
-      render({ data }) {
-        return `Success!`;
-      },
-      autoClose: 3000,
-      pauseOnHover: false,
-    },
-    pending: `Waiting for txn ${txHash} to confirm`,
-    error: {
-      render({ data }) {
-        return `Error confirming txn: ${data.message}!`;
-      },
-      autoClose: 3000,
-      pauseOnHover: false,
-    },
-  });
-};
